99d40966f95f81e556f375402a53efd56a885b10
MuhammadWaseem5196 <159682025+MuhammadWaseem5196@users.noreply.github.com>
2024-12-14 12:19:00 +0500

    created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
+
 
 import Hero from "@/components/hero-section/hero";
 
 export default function Home() {
   return (
-    <div >
-      <Hero></Hero>
-      {/* /categories */}
-      {/* /detail */}
+    <div>
+      <Hero/>
     </div>
   );
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link"; // Import the Link component from Next.js
 
 const Footer = () => {
   return (
@@ -18,24 +19,24 @@ const Footer = () => {
           <h2 className="font-bold mb-1">About</h2>
           <ul>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                How it works
-              </a>
+              <Link href="#">
+                <span className="hover:underline">How it works</span>
+              </Link>
             </li>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Featured
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Featured</span>
+              </Link>
             </li>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Partnership
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Partnership</span>
+              </Link>
             </li>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Business Relation
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Business Relation</span>
+              </Link>
             </li>
           </ul>
         </div>
@@ -43,24 +44,24 @@ const Footer = () => {
           <h2 className="font-bold mb-1">Community</h2>
           <ul>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Events
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Events</span>
+              </Link>
             </li>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Blog
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Blog</span>
+              </Link>
             </li>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Podcast
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Podcast</span>
+              </Link>
             </li>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Invite a friend
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Invite a friend</span>
+              </Link>
             </li>
           </ul>
         </div>
@@ -68,24 +69,24 @@ const Footer = () => {
           <h2 className="font-bold mb-1">Socials</h2>
           <ul>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Discord
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Discord</span>
+              </Link>
             </li>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Instagram
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Instagram</span>
+              </Link>
             </li>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Twitter
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Twitter</span>
+              </Link>
             </li>
             <li className="mb-1">
-              <a href="#" className="hover:underline">
-                Facebook
-              </a>
+              <Link href="#">
+                <span className="hover:underline">Facebook</span>
+              </Link>
             </li>
           </ul>
         </div>
@@ -96,12 +97,12 @@ const Footer = () => {
           <p>© 2022 MORENT. All rights reserved</p>
         </div>
         <div className="flex space-x-4 mt-2 lg:mt-0">
-          <a href="#" className="hover:underline">
-            Privacy Policy
-          </a>
-          <a href="#" className="hover:underline">
-            Terms & Condition
-          </a>
+          <Link href="#">
+            <span className="hover:underline">Privacy Policy</span>
+          </Link>
+          <Link href="#">
+            <span className="hover:underline">Terms & Condition</span>
+          </Link>
         </div>
       </div>
     </footer>
@@ -109,4 +110,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
